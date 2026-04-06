1ce1be43995d1d91ff2e58bac45d465288d0b1d3
shivam sharma <shivamsxarma@gmail.com>
2026-04-06 16:41:25 +0530

    new update

--- DOM_manupulation/Day4/index5.js
+++ DOM_manupulation/Day4/index5.js
@@ -1,5 +1,39 @@
-let h1 = document.querySelector('h1');
-console.log(h1.tagName) //tag return karta haii
-console.log(h1.nodeName) // tagname , text node , comment node.
+// // let h1 = document.querySelector('h1');
+// // console.log(h1.tagName) //tag return karta haii
+// // console.log(h1.nodeName) // tagname , text node , comment node.
 
-console.log(h1.firstChild)
+// // console.log(h1.firstChild)
+// // console.log(h1.innerText)
+// // console.log(h1.textContent) with pure formated text
+
+// // h1.innerText= "hellow aa gaya"
+
+// // console.log(h1.innerText)
+// // console.log(h1.textContent) 
+
+// // h1.innerHTML = '<p>abb to clear haii</p>'
+// // console.log(h1.innerText)
+// // console.log(h1.textContent) 
+
+// //inserting element
+
+// let div = document.querySelector('div');
+// let p = document.createElement('p')
+// p.innerText = "New para append"
+// // div.append(p)
+// // div.prepend(p)
+// // div.after(p)
+// div.before(p)
+
+// let selection = document.querySelector('#selection')
+// selection.style.backgroundColor = 'blue';
+
+// let parentNode = selection.parentNode
+// parentNode.append(p) 
+
+
+// let newElement = document.createElement('div');
+// newElement.innerText = "hellow";
+
+// parentNode.replaceChild(newElement,selection)
+
