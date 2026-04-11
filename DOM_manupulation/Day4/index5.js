2d1c14e1ab38efb08c0a590999dac1f24dc5740c
shivam sharma <shivamsxarma@gmail.com>
2026-04-11 11:47:04 +0530

    updated code

--- DOM_manupulation/Day4/index5.js
+++ DOM_manupulation/Day4/index5.js
@@ -1,39 +1,68 @@
-// // let h1 = document.querySelector('h1');
-// // console.log(h1.tagName) //tag return karta haii
-// // console.log(h1.nodeName) // tagname , text node , comment node.
+// // Source - https://stackoverflow.com/a/58423796
+// // Posted by Matt Bierner, modified by community. See post 'Timeline' for change history
+// // Retrieved 2026-04-06, License - CC BY-SA 4.0
 
-// // console.log(h1.firstChild)
-// // console.log(h1.innerText)
-// // console.log(h1.textContent) with pure formated text
-
-// // h1.innerText= "hellow aa gaya"
-
-// // console.log(h1.innerText)
-// // console.log(h1.textContent) 
-
-// // h1.innerHTML = '<p>abb to clear haii</p>'
-// // console.log(h1.innerText)
-// // console.log(h1.textContent) 
-
-// //inserting element
-
-// let div = document.querySelector('div');
-// let p = document.createElement('p')
-// p.innerText = "New para append"
-// // div.append(p)
-// // div.prepend(p)
-// // div.after(p)
-// div.before(p)
-
-// let selection = document.querySelector('#selection')
-// selection.style.backgroundColor = 'blue';
-
-// let parentNode = selection.parentNode
-// parentNode.append(p) 
+// /** @type {HTMLDivElement} */
+/** @type {HTMLInputElement} */
 
 
-// let newElement = document.createElement('div');
-// newElement.innerText = "hellow";
+// const listElement =  document.querySelector('.list');
+// listElement.style
 
-// parentNode.replaceChild(newElement,selection)
 
+// // // let h1 = document.querySelector('h1');
+// // // console.log(h1.tagName) //tag return karta haii
+// // // console.log(h1.nodeName) // tagname , text node , comment node.
+
+// // // console.log(h1.firstChild)
+// // // console.log(h1.innerText)
+// // // console.log(h1.textContent) with pure formated text
+
+// // // h1.innerText= "hellow aa gaya"
+
+// // // console.log(h1.innerText)
+// // // console.log(h1.textContent) 
+
+// // // h1.innerHTML = '<p>abb to clear haii</p>'
+// // // console.log(h1.innerText)
+// // // console.log(h1.textContent) 
+
+// // //inserting element
+
+// // let div = document.querySelector('div');
+// // let p = document.createElement('p')
+// // p.innerText = "New para append"
+// // // div.append(p)
+// // // div.prepend(p)
+// // // div.after(p)
+// // div.before(p)
+
+// let selection = (document.getElementById('#selection'))
+
+// // selection.style.backgroundColor = 'blue';
+
+// // let parentNode = selection.parentNode
+// // parentNode.append(p) 
+
+
+// // let newElement = document.createElement('div');
+// // newElement.innerText = "hellow";
+
+// // parentNode.replaceChild(newElement,selection)
+
+// let div = document.querySelector('.div')
+
+// console.log(div.attributes)
+
+// /** @type {HTMLInputElement} */
+// const myInput = document.querySelector('.my-input');
+// myInput.style
+// // Now myInput.value will show in suggestions
+
+
+
+// let a = document.getElementById('div')
+// a.style.backgroundColor='green'
+
+let a = document.querySelector('.div')
+a.style.backgroundColor='green'
